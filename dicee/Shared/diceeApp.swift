3385c5c982161ca6260ea2cfddc17a0a4dcd9387
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-05-18 18:51:38 -0700

    Add files via upload

---
+++ dicee/Shared/diceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceeApp.swift
+//  Shared
+//
+//  Created by a-robota on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct diceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
